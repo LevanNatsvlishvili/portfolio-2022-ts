f515cceeec2d69ebea656786ee5a8d44457752d5
unknown <Ln@leavingstone.com>
2023-04-22 20:02:35 +0400

    test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import Layout from 'components/Layout';
 import type { AppProps } from 'next/app';
-import 'styles/global.css';
 import { StoreContextProvider } from 'utils/Store';
+import 'styles/global.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
