ad50ffd904e0d96dc01fc4ebfbc7bea0ae2286d7
Levan Natsvlishvili <L.nats51@yahoo.com>
2024-07-17 21:45:20 +0400

    changed slider to older version with improvements

--- components/Pages/Home/Projects/Slider.tsx
+++ components/Pages/Home/Projects/Slider.tsx
@@ -1,55 +1,59 @@
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Pagination, Navigation, FreeMode, Autoplay } from 'swiper/modules';
-import SwiperCore from 'swiper';
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
-import Image from 'next/image';
-import { memo } from 'react';
-
-SwiperCore.use([FreeMode, Autoplay]);
+/* eslint-disable @next/next/no-img-element */
+import React from 'react';
 
 const images = [
   { id: 0, img: '/images/projects/citrus.png' },
   { id: 1, img: '/images/projects/dataninja.png' },
   { id: 2, img: '/images/projects/tourism.png' },
   { id: 3, img: '/images/projects/clothingstore.png' },
+  { id: 4, img: '/images/projects/portfolio.png' },
   { id: 5, img: '/images/projects/nbg.png' },
   { id: 6, img: '/images/projects/marsleaders.png' },
   { id: 7, img: '/images/projects/archi.png' },
 ];
 
-interface SliderProps {
+interface Slider {
   shouldDisplay: boolean;
 }
 
-const Slider = ({ shouldDisplay }: SliderProps) => {
+function Slider(props: Slider) {
+  const { shouldDisplay } = props;
+
   return (
     <div
       className={`w-[90%] sm:w-3/4 lg:w-1/2 h-[70%] lg:h-1/2 lg:ml-100 relative glass-effect overflow-hidden rounded-xl ${
-        shouldDisplay && 'glass-effect-active'
-      }`}
+        shouldDisplay ? 'glass-effect-active' : ''
+      } `}
     >
-      <Swiper
-        freeMode={true}
-        slidesPerView={'auto'}
-        autoplay={{
-          delay: 2000,
-          disableOnInteraction: false,
-          pauseOnMouseEnter: false,
-        }}
-        speed={1000} // Adjust the speed to control how fast the slider moves
-        loop={true}
-        className="h-full"
-      >
-        {images.map((image) => (
-          <SwiperSlide key={image.id}>
-            <Image className="w-full h-full" src={image.img} alt="project" loading="lazy" fill />
-          </SwiperSlide>
+      <div style={{ width: `${images.length}00%` }} className="flex transition slider-auto h-full">
+        {images.reverse().map((image, index) => (
+          <img key={index} className="w-[850px] h-full " src={image.img} alt="project" />
         ))}
-      </Swiper>
+      </div>
+
+      <style jsx global>
+        {`
+          @-webkit-keyframes translateinfinite {
+            0% {
+              transform: translateX(0);
+            }
+            100% {
+              transform: translateX(calc(-100%));
+            }
+          }
+
+          @keyframes translateinfinite {
+            0% {
+              transform: translateX(0);
+            }
+            100% {
+              transform: translateX(calc(850px * -${images.length - 1}));
+            }
+          }
+        `}
+      </style>
     </div>
   );
-};
+}
 
-export default memo(Slider);
+export default Slider;
