7581e958b649032645f07f6cb333f07c209771ab
Levan Natsvlishvili <L.nats51@yahoo.com>
2024-07-23 19:39:42 +0400

    optimzation refactor

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, useEffect } from 'react';
+import React, { ReactNode, useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import useStore from 'utils/Store/Context';
 import Loading from 'components/Components/Loading';
@@ -19,9 +19,26 @@ interface Layout {
   children: ReactNode;
 }
 
+const checkDeviceStrength = () => {
+  const cores = navigator.hardwareConcurrency || 1;
+  const memory = (navigator as any).deviceMemory || 4;
+  console.log('memory', memory);
+  console.log('cores', cores);
+  return cores >= 4 && memory >= 8;
+};
+
 const Layout = ({ children }: Layout) => {
   const { currView, setCurrView, loading } = useStore();
   const shouldScrollDisplay = useQuery();
+
+  const [isHighPerformance, setIsHighPerformance] = useState(false);
+
+  useEffect(() => {
+    const isStrongDevice = checkDeviceStrength();
+    setIsHighPerformance(isStrongDevice);
+  }, []);
+
+  console.log(isHighPerformance);
 
   useEffect(() => {
     if (shouldScrollDisplay) {
